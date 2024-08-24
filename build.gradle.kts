4ef2c5ba39a64addd2d1c99e518d706ce69afa78
mohamedsallam953 <mohamed@tawk.to>
2024-08-24 18:15:02 +0300

    add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,21 @@ plugins {
     id("com.google.dagger.hilt.android") version libs.versions.hiltVersion apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("androidx.room") version libs.versions.roomVersion apply false
+    id("androidx.room") version libs.versions.roomVersion
+    alias(libs.plugins.detektPlugin) version libs.versions.detektPluginVersion
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+        toolVersion = "1.23.1" // Specify the version of Detekt
+        input.setFrom(files("src/main/java", "src/main/kotlin")) // Specify input directories
+        //  config.setFrom(files("$rootDir/config/detekt/detekt.yml")) // Path to Detekt configuration file (optional)
+        reports {
+            xml.required.set(true) // Output the result in XML format
+            html.required.set(true) // Output the result in HTML format
+        }
+    }
 }
 
